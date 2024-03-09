c9b6fa91cc40b943a1af624b05f9de938e383fb2
Sarnava <sbsarnava@gmail.com>
2024-03-09 19:48:38 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AdmissionsComponent } from './admissions/admissions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'admissions', component: AdmissionsComponent },
+];
